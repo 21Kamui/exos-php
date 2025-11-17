ed1c324f6d197505a70c10a597e5de730999ef38
21Kamui <ilyas211022@gmail.com>
2025-11-17 12:42:15 +0100

    Add files via upload

--- traitement.php
+++ traitement.php
@@ -1,17 +1,17 @@
 <?php
-
 function ecole ($age) {
-    if $age <= 3 {
+    if ($age <= 3) {
         return "creche";
-    } elseif $age > 3 && $age <= 6 {
+    } elseif ($age > 3 && $age <= 6) {
         return "maternelle";
-    } elseif $age > 6 && $age <= 11 {
+    } elseif ($age > 6 && $age <= 11) {
+        return "primaire";
+    } elseif ($age > 11 && $age <= 15) {
         return "college";
-    } elseif $age > 11 && $age <= 16 {
-        return "college";
-    } elseif $age > 16 && $age <= 18 {
+    } elseif ($age > 15 && $age <= 18) {
         return "lycee";
     } else {
         return "";
     }
 }
+echo ecole(4);
