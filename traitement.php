8950720c61dcae74d9d55cb7674eb2e93f6bb474
Kamui _tv <ilyas211022@gmail.com>
2025-11-17 12:18:55 +0100

    test

---
+++ traitement.php
@@ -0,0 +1,17 @@
+<?php
+
+function ecole ($age) {
+    if $age <= 3 {
+        return "creche";
+    } elseif $age > 3 && $age <= 6 {
+        return "maternelle";
+    } elseif $age > 6 && $age <= 11 {
+        return "college";
+    } elseif $age > 11 && $age <= 16 {
+        return "college";
+    } elseif $age > 16 && $age <= 18 {
+        return "lycee";
+    } else {
+        return "";
+    }
+}
