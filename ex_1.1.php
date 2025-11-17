6515d167d98cdcfc5dd00aa51042193297cb7645
21Kamui <ilyas211022@gmail.com>
2025-11-17 12:49:27 +0100

    Add files via upload

---
+++ ex_1.1.php
@@ -0,0 +1,17 @@
+<?php
+function ecole ($age) {
+    if ($age <= 3) {
+        return "creche";
+    } elseif ($age > 3 && $age <= 6) {
+        return "maternelle";
+    } elseif ($age > 6 && $age <= 11) {
+        return "primaire";
+    } elseif ($age > 11 && $age <= 15) {
+        return "college";
+    } elseif ($age > 15 && $age <= 18) {
+        return "lycee";
+    } else {
+        return "";
+    }
+}
+echo ecole(4);
