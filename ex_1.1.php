04a7be2d98110ac4da671b4247d309a364aa9b9c
Kamui _tv <ilyas211022@gmail.com>
2025-11-18 11:35:53 +0100

    test

--- ex_1.1.php
+++ ex_1.1.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 function ecole ($age) {
     if ($age <= 3) {
@@ -15,22 +14,4 @@ function ecole ($age) {
         return "";
     }
 }
-=======
-<?php
-function ecole ($age) {
-    if ($age <= 3) {
-        return "creche";
-    } elseif ($age > 3 && $age <= 6) {
-        return "maternelle";
-    } elseif ($age > 6 && $age <= 11) {
-        return "primaire";
-    } elseif ($age > 11 && $age <= 15) {
-        return "college";
-    } elseif ($age > 15 && $age <= 18) {
-        return "lycee";
-    } else {
-        return "";
-    }
-}
->>>>>>> 37c1093376f70ff1e0e5eeb47607e10bec4b21b0
 echo ecole(4);
